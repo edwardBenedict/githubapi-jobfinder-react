b91598fcf52e977442a04dca523ac7c0e906c9df
ed ben <edwardbenedict92@gmail.com>
2021-05-04 18:35:39 +0300

    feat: add loading and 404

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import axios from "axios";
 import JobCard from "./components/JobCard";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import fof from "./assets/404.png";
+import loadingGif from "./assets/loading.gif";
 
 function App() {
   const [info, setInfo] = useState({ description: "", location: "" });
   const [jobs, setJobs] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   const getInfo = () => {
+    setIsLoading(true);
     axios({
       method: "get",
       url: `/positions.json?description=${info.description}&location=${info.location}`,
@@ -17,9 +21,11 @@ function App() {
       .then(function (response) {
         console.log(response.data);
         setJobs(response.data);
+        setIsLoading(false);
       })
       .catch(function (error) {
         console.log(error);
+        setIsLoading(false);
       });
   };
 
@@ -33,17 +39,23 @@ function App() {
     <div className="App">
       <Header />
       <Form setInfo={setInfo} info={info} getJobs={getJobs} />
-      {jobs?.map((job, index) => (
-        <JobCard
-          key={index}
-          company={job?.company}
-          company_logo={job?.company_logo}
-          company_url={job?.company_url}
-          location={job?.location}
-          type={job?.type}
-          title={job?.title}
-        />
-      ))}
+      {isLoading ? (
+        <img src={loadingGif} style={{ width: "100px" }} alt="404" />
+      ) : jobs?.length === 0 ? (
+        <img src={fof} style={{ width: "500px" }} alt="404" />
+      ) : (
+        jobs?.map((job, index) => (
+          <JobCard
+            key={index}
+            company={job?.company}
+            company_logo={job?.company_logo}
+            company_url={job?.company_url}
+            location={job?.location}
+            type={job?.type}
+            title={job?.title}
+          />
+        ))
+      )}
     </div>
   );
 }
